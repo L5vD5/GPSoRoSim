dc97af5d1615a922b25007808700ce79a5f1935d
l5vd5 <l5vd5@korea.ac.kr>
2022-04-29 09:16:46 +0900

    add logging

--- 4dof/eval.py
+++ 4dof/eval.py
@@ -4,16 +4,20 @@ import numpy as np
 from sklearn import gaussian_process
 import random
 import json
+import os
 
-train_path = '/home/yoonbyung/Dev/GPSoRoSim/data/4dof/trainInterpolate.json'
+train_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/4dof/trainInterpolate.json')
 with open(train_path, "r") as st_json:
 
     train_json = json.load(st_json)['data']
 
-test_path = '/home/yoonbyung/Dev/GPSoRoSim/data/4dof/valInterpolate.json'
+test_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/4dof/valExtrapolate.json')
 with open(test_path, "r") as st_json:
 
     test_json = json.load(st_json)['data']
+    
+print('num of train data {}'.format(len(train_json)))
+print('num of test data {}'.format(len(test_json)))
 
 train_X = np.empty(shape=(0,4))
 train_y = np.empty(shape=(0,9,3))
@@ -27,16 +31,14 @@ for data in test_json:
   test_X = np.concatenate((test_X, [data['actuation']]), axis=0)
   test_y = np.concatenate((test_y, [data['position']]), axis=0)
 
+print('maximum train X {}'.format(np.max(train_X)))
+print('maximum test X {}'.format(np.max(test_X)))
 fig = plt.figure()
-# fig2 = plt.figure()
 
 ax = fig.add_subplot(projection='3d')
 ax = fig.add_subplot(projection='3d')
 ax.set_title('GT')
-# ax2.set_title('Predicted')
 
-# print(test_X.shape)
-# print(test_y.shape)
 train_X = train_X[0:100]
 train_y = train_y[0:100]
 GPR = gaussian_process.GaussianProcessRegressor(kernel=gaussian_process.kernels.RBF(length_scale=1.2))
@@ -57,15 +59,8 @@ def update2(frame, data2, line2):
   return line2
 
 
-# ax.set_xlim3d([-10, 10])
-# ax.set_ylim3d([-10, 10])
-# ax.set_zlim3d([-10, 10])
-# ax2.set_xlim3d([-10, 10])
-# ax2.set_ylim3d([-10, 10])
-# ax2.set_zlim3d([-10, 10])
 pred = pred.reshape(-1, 9, 3)
-# print(np.argmax((test_y - pred)/test_y))
-# print(test_y.shape)
+print(np.max((test_y - pred)/test_y))
 pred += [0.1, 0, 0]
 
 line = ax.plot(test_y[0, :, 0], test_y[0, :, 1], test_y[0, :, 2], '.-')
